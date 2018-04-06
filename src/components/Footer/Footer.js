dd721bdb8146b2b931bac57aa506abd214ec6283
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-04-06 21:07:15 +0200

    style(Footer.js): add some styling to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,27 +10,35 @@ const Container = styled.footer`
 `;
 
 const Top = styled.div`
-  text-align: right;
+  display: flex;
   color: #999;
   background-color: #222;
   background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNTIiIGhlaWdodD0iMTUyIiB2aWV3Qm94PSIwIDAgMTUyIDE1MiI+PGcgZmlsbC1ydWxlPSJldmVub2RkIj48ZyBpZD0idGVtcGxlIiBmaWxsPSIjMDAwIj48cGF0aCBkPSJNMTUyIDE1MHYySDB2LTJoMjh2LThIOHYtMjBIMHYtMmg4VjgwaDQydjIwaDIwdjQySDMwdjhoOTB2LThIODB2LTQyaDIwVjgwaDQydjQwaDhWMzBoLTh2NDBoLTQyVjUwSDgwVjhoNDBWMGgydjhoMjB2MjBoOFYwaDJ2MTUwem0tMiAwdi0yOGgtOHYyMGgtMjB2OGgyOHpNODIgMzB2MThoMThWMzBIODJ6bTIwIDE4aDIwdjIwaDE4VjMwaC0yMFYxMEg4MnYxOGgyMHYyMHptMCAydjE4aDE4VjUwaC0xOHptMjAtMjJoMThWMTBoLTE4djE4em0tNTQgOTJ2LTE4SDUwdjE4aDE4em0tMjAtMThIMjhWODJIMTB2MzhoMjB2MjBoMzh2LTE4SDQ4di0yMHptMC0yVjgySDMwdjE4aDE4em0tMjAgMjJIMTB2MThoMTh2LTE4em01NCAwdjE4aDM4di0yMGgyMFY4MmgtMTh2MjBoLTIwdjIwSDgyem0xOC0yMEg4MnYxOGgxOHYtMTh6bTItMmgxOFY4MmgtMTh2MTh6bTIwIDQwdi0xOGgxOHYxOGgtMTh6TTMwIDBoLTJ2OEg4djIwSDB2Mmg4djQwaDQyVjUwaDIwVjhIMzBWMHptMjAgNDhoMThWMzBINTB2MTh6bTE4LTIwSDQ4djIwSDI4djIwSDEwVjMwaDIwVjEwaDM4djE4ek0zMCA1MGgxOHYxOEgzMFY1MHptLTItNDBIMTB2MThoMThWMTB6Ii8+PC9nPjwvZz48L3N2Zz4=);
   ul {
     list-style: none;
     margin: 0;
-    li:first-child {
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  a:hover {
+    color: #fff;
+  }
+  & > div {
+    flex: 1;
+  }
+  & > ul {
+    text-align: right;
+    display: flex;
+    flex: 1;
+    & > li {
+      flex: 1;
+    }
+    & > li > a {
       color: #ddd;
       ${scaleSC(0.5)};
       font-weight: 700;
-    }
-    li {
-      margin: 0;
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-      a:hover {
-        color: #fff;
-      }
     }
   }
 `;
@@ -61,6 +69,12 @@ const Bottom = styled.div`
 const Footer = () => (
   <Container>
     <Top>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt molestias saepe assumenda culpa adipisci
+        repellat illum aperiam expedita quos ipsa beatae fugiat delectus, dolorem, ex placeat impedit fuga. Voluptas,
+        deserunt laborum? Incidunt, vero neque libero quos officia error tempora similique consequuntur veritatis, vitae
+        odit optio nemo id, exercitationem recusandae?
+      </div>
       <ul>
         <li>
           <Link to="/">lorem</Link>
