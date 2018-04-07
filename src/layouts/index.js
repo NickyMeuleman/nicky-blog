0d87c5d124781435f967febd551e7284f6c8d25a
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-04-07 17:25:30 +0200

    style: header styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,13 +4,16 @@ import Helmet from 'react-helmet';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import './index.css';
+import favicon from '../../static/favicon.png';
 
 const TemplateWrapper = ({ children }) => (
   <React.Fragment>
     <Helmet
       title="Nicky blogs"
       meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
-    />
+    >
+      <link rel="icon" type="image/png" sizes="64x64" href={favicon} />
+    </Helmet>
     <Header />
     <main>{children()}</main>
     <Footer />
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 
 export default class AboutPage extends Component {
   render() {
-    return <div />;
+    return (
+      <React.Fragment>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facilis corrupti placeat pariatur totam
+          iure, ad eos sapiente odit excepturi tenetur? Ipsam nobis id quibusdam suscipit quidem omnis nemo ab ex
+          corporis, sunt nulla, fugit, nesciunt magnam quis facere corrupti praesentium dicta cum cupiditate eius. Quo
+          nostrum velit officia, error laboriosam beatae qui saepe libero veniam excepturi architecto temporibus,
+          necessitatibus tempore id numquam iste fugit? Aliquid, architecto, quia saepe dolorum eos atque perferendis
+          consequatur hic nulla exercitationem ab neque debitis corrupti aliquam tempora. Blanditiis iure praesentium
+          iste, porro expedita, quam voluptatibus aspernatur a quia incidunt consectetur ullam impedit veritatis aliquam
+          vitae maiores eum temporibus, architecto omnis saepe dignissimos exercitationem obcaecati. Expedita mollitia
+          beatae officiis tempore atque, assumenda nemo iusto dolorem molestiae omnis in nihil et repellat? Modi omnis
+          ullam a sequi dolore commodi, inventore fugiat optio sunt ut? Esse, vitae! Deserunt impedit velit nihil. Ad
+          nam nisi officia voluptates excepturi.
+        </p>
+      </React.Fragment>
+    );
   }
 }
