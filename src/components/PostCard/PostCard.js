da1d2d071cd580b37dfbc4f77beb67474fb2378c
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-04-21 19:47:40 +0200

    feat: restyle blog/ page start

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
-
 import Img from 'gatsby-image';
-import './PostCard.css';
+import styled from 'styled-components';
+import { rhythm } from '../../utils/typography';
+
+const LinkU = styled(Link)`
+  border-bottom: none;
+  box-shadow: none;
+`;
+
+const Card = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 250px 100px;
+`;
+const Top = styled.div`
+  grid-column: 1/2;
+  grid-row: 1/2;
+  background: green;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+  img {
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+  }
+  ${Card}:hover & {
+    filter: brightness(110%) contrast(120%);
+  }
+`;
+const Bottom = styled.div`
+  grid-column: 1/2;
+  grid-row: 2/3;
+  background: #fff;
+`;
 
 export default class PostCard extends Component {
   render() {
-    console.log('blogpage props: ', this.props);
     return (
-      <div className="card">
-        <Link to={this.props.url}>
-          <div className="card__image">{this.props.coverSizes && <Img sizes={this.props.coverSizes} />}</div>
-          <div className="card__content">
-            <h3 className="card__title">{this.props.title}</h3>
-            <p className="card__more">{this.props.date}</p>
-          </div>
-        </Link>
-      </div>
+      <LinkU to={this.props.url}>
+        <Card>
+          <Top>{this.props.coverSizes && <Img sizes={this.props.coverSizes} />}</Top>
+          <Bottom>{this.props.title}</Bottom>
+        </Card>
+      </LinkU>
     );
   }
 }
+
+// export default class PostCard extends Component {
+//   render() {
+//     console.log('blogpage props: ', this.props);
+//     return (
+//       <div className="card">
+//         <Link to={this.props.url}>
+//           <div className="card__image">{this.props.coverSizes && <Img sizes={this.props.coverSizes} />}</div>
+//           <div className="card__content">
+//             <h3 className="card__title">{this.props.title}</h3>
+//             <p className="card__more">{this.props.date}</p>
+//           </div>
+//         </Link>
+//       </div>
+//     );
+//   }
+// }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 import Hero from '../components/Hero/Hero';
 import PostCard from '../components/PostCard/PostCard';
+import { rhythm } from '../utils/typography';
+
+const Container = styled.div`
+  margin: 0 ${rhythm(1)} ${rhythm(0.5)} ${rhythm(1)};
+  @media (min-width: 55rem) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto;
+    grid-gap: ${rhythm(1)};
+    margin: 0 ${rhythm(2)} ${rhythm(1.5)} ${rhythm(2)};
+  }
+  @media (min-width: 70rem) {
+    grid-template-columns: 1fr 1fr;
+    margin: 0 ${rhythm(3)} ${rhythm(2)} ${rhythm(3)};
+  }
+`;
 
 const PostsPage = ({ data }) => (
   <div>
     <Hero title="Writing things down" />
     <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-      <PostCard
-        key={node.id}
-        url={`/blog${node.fields.slug}`}
-        title={node.frontmatter.title}
-        date={node.frontmatter.date}
-        coverSizes={node.frontmatter.cover ? node.frontmatter.cover.childImageSharp.sizes : null}
-      />
-    ))}
+    <Container>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <PostCard
+          key={node.id}
+          url={`/blog${node.fields.slug}`}
+          title={node.frontmatter.title}
+          date={node.frontmatter.date}
+          coverSizes={node.frontmatter.cover ? node.frontmatter.cover.childImageSharp.sizes : null}
+        />
+      ))}
+    </Container>
   </div>
 );
 
