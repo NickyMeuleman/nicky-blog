3131cf3ff99446d41c17512e17e224b6792143de
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-04-26 18:22:15 +0200

    refactor(Footer.js): minor changes to content and layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -22,7 +22,7 @@ const Container = styled.footer`
 
 const Top = styled.div`
   display: grid;
-  grid-template-rows: ${rhythm(1)} 1fr ${rhythm(1)};
+  grid-template-rows: ${rhythm(0.5)} 1fr ${rhythm(0.5)};
   grid-template-columns: ${rhythm(1)} 1fr 1fr ${rhythm(1)};
   color: #999;
   background-color: #222;
@@ -37,7 +37,10 @@ const Top = styled.div`
   & > div {
     grid-column: 2/4;
     grid-row: 2/3;
-    align-self: start;
+    align-self: center;
+    p {
+      margin-bottom: ${rhythm(0.25)};
+    }
   }
   & > ul {
     text-align: right;
@@ -126,49 +129,23 @@ const Bottom = styled.div`
   }
 `;
 
-// const Bottom = styled.div`
-//   ${scaleSC(-0.25)};
-//   background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNTIiIGhlaWdodD0iMTUyIiB2aWV3Qm94PSIwIDAgMTUyIDE1MiI+PGcgZmlsbC1ydWxlPSJldmVub2RkIj48ZyBpZD0idGVtcGxlIiBmaWxsPSIjMDAwIj48cGF0aCBkPSJNMTUyIDE1MHYySDB2LTJoMjh2LThIOHYtMjBIMHYtMmg4VjgwaDQydjIwaDIwdjQySDMwdjhoOTB2LThIODB2LTQyaDIwVjgwaDQydjQwaDhWMzBoLTh2NDBoLTQyVjUwSDgwVjhoNDBWMGgydjhoMjB2MjBoOFYwaDJ2MTUwem0tMiAwdi0yOGgtOHYyMGgtMjB2OGgyOHpNODIgMzB2MThoMThWMzBIODJ6bTIwIDE4aDIwdjIwaDE4VjMwaC0yMFYxMEg4MnYxOGgyMHYyMHptMCAydjE4aDE4VjUwaC0xOHptMjAtMjJoMThWMTBoLTE4djE4em0tNTQgOTJ2LTE4SDUwdjE4aDE4em0tMjAtMThIMjhWODJIMTB2MzhoMjB2MjBoMzh2LTE4SDQ4di0yMHptMC0yVjgySDMwdjE4aDE4em0tMjAgMjJIMTB2MThoMTh2LTE4em01NCAwdjE4aDM4di0yMGgyMFY4MmgtMTh2MjBoLTIwdjIwSDgyem0xOC0yMEg4MnYxOGgxOHYtMTh6bTItMmgxOFY4MmgtMTh2MTh6bTIwIDQwdi0xOGgxOHYxOGgtMTh6TTMwIDBoLTJ2OEg4djIwSDB2Mmg4djQwaDQyVjUwaDIwVjhIMzBWMHptMjAgNDhoMThWMzBINTB2MTh6bTE4LTIwSDQ4djIwSDI4djIwSDEwVjMwaDIwVjEwaDM4djE4ek0zMCA1MGgxOHYxOEgzMFY1MHptLTItNDBIMTB2MThoMThWMTB6Ii8+PC9nPjwvZz48L3N2Zz4=);
-//   display: grid;
-//   grid-template-rows: ${rhythm(0.5)} 1fr ${rhythm(0.5)};
-//   grid-template-columns: ${rhythm(4)} 1fr 1fr ${rhythm(4)};
-//   background-color: #111;
-//   color: #777;
-//   & > nav {
-//     grid-column: 2/3;
-//     grid-row: 2/3;
-//     justify-self: start;
-//   }
-//   & > div {
-//     grid-column: 3/4;
-//     grid-row: 2/3;
-//     justify-self: end;
-//   }
-//   ul {
-//     list-style: none;
-//     display: flex;
-//     margin: 0;
-//     li {
-//       margin: 0;
-//       margin-right: ${rhythm(3 / 4)};
-//     }
-//   }
-// `;
-
 const Footer = () => (
   <Container>
     <Top>
       <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt molestias saepe assumenda culpa adipisci
-        repellat illum aperiam expedita quos ipsa beatae fugiat delectus, dolorem, ex placeat impedit fuga. Voluptas,
-        deserunt laborum?
+        <p>Is there something awesome related to JavaScript you want to talk about?</p>
+        <p>Do you want to write a blogpost for this site?</p>
+        <p>Excited about racecars?</p>
+        <p>
+          <Link to="/about">Get in touch!</Link>
+        </p>
       </div>
       <ul>
         <li>
-          <Link to="/">Rook</Link>
+          <Link to="/">Home</Link>
           <ul>
             <li>
-              <Link to="/posts">Posts</Link>
+              <Link to="/blog">Blog</Link>
             </li>
             <li>
               <Link to="/about">About</Link>
@@ -177,13 +154,17 @@ const Footer = () => (
         </li>
 
         <li>
-          <Link to="/">Contact</Link>
+          <Link to="/about">Contact</Link>
           <ul>
             <li>
-              <a href="https://github.com/NickyMeuleman">Github</a>
+              <a href="https://github.com/NickyMeuleman" target="_blank" rel="noopener noreferrer">
+                Github
+              </a>
             </li>
             <li>
-              <a href="https://twitter.com/NMeuleman">Twitter</a>
+              <a href="https://twitter.com/NMeuleman" target="_blank" rel="noopener noreferrer">
+                Twitter
+              </a>
             </li>
             <li>
               <a href="mailto:nicky.meuleman+dev@gmail.com">E-mail</a>
@@ -196,17 +177,20 @@ const Footer = () => (
       <nav>
         <ul>
           <li>
-            <Link to="/">Contact</Link>
+            Powered by{' '}
+            <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">
+              Gatsby
+            </a>
           </li>
           <li>
-            <Link to="/">Posts</Link>
-          </li>
-          <li>
-            <Link to="/">About</Link>
+            Hosted on{' '}
+            <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">
+              Netlify
+            </a>
           </li>
         </ul>
       </nav>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+      <div>Designed and developed by Nicky Meuleman</div>
     </Bottom>
   </Container>
 );
