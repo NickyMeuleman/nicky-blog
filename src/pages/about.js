26ed568d4be17319ed3eaa5560e47e2ad61d1a9b
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-08-01 00:13:17 +0200

    feat(about): init layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,60 @@
 import React from 'react';
-import Hero from '../components/Hero/Hero';
+import styled from 'styled-components';
 import Layout from '../components/Layout/Layout';
+import { rhythm } from '../utils/typography';
+
+const Container = styled.div`
+  flex: 1 0 auto;
+  display: grid;
+  grid-template-columns: 1fr minmax(70vw, 5fr) 1fr;
+  grid-template-rows: 15vh 1fr minmax(${rhythm(1)}, 1fr);
+`;
+
+const Triangle = styled.div`
+  grid-column: 1/-1;
+  grid-row: 1/-1;
+  background: ${props => `linear-gradient(120deg, ${props.theme.primary} -50%, ${props.theme.secondary})`};
+  clip-path: polygon(0 0, 0% 50vh, 70vw 0);
+  z-index: 2;
+  position: relative;
+  & h1 {
+    margin: 0;
+    position: absolute;
+    top: 5vh;
+    left: 5vw;
+    color: #f5f5f5;
+  }
+`;
+
+const Content = styled.div`
+  grid-column: 2/-2;
+  grid-row: 2/-2;
+  align-self: start;
+  z-index: 3;
+  box-shadow: 0px 3px 25px #333;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: ${rhythm(1)};
+`;
 
 const AboutPage = () => (
   <Layout>
-    <Hero title="About me" />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facilis corrupti placeat pariatur totam iure,
-      ad eos sapiente odit excepturi tenetur? Ipsam nobis id quibusdam suscipit quidem omnis nemo ab ex corporis, sunt
-      nulla, fugit, nesciunt magnam quis facere corrupti praesentium dicta cum cupiditate eius. Quo nostrum velit
-      officia, error laboriosam beatae qui saepe libero veniam excepturi architecto temporibus, necessitatibus tempore
-      id numquam iste fugit? Aliquid, architecto, quia saepe dolorum eos atque perferendis consequatur hic nulla
-      exercitationem ab neque debitis corrupti aliquam tempora. Blanditiis iure praesentium iste, porro expedita, quam
-      voluptatibus aspernatur a quia incidunt consectetur ullam impedit veritatis aliquam vitae maiores eum temporibus,
-      architecto omnis saepe dignissimos exercitationem obcaecati. Expedita mollitia beatae officiis tempore atque,
-      assumenda nemo iusto dolorem molestiae omnis in nihil et repellat? Modi omnis ullam a sequi dolore commodi,
-      inventore fugiat optio sunt ut? Esse, vitae! Deserunt impedit velit nihil. Ad nam nisi officia voluptates
-      excepturi.
-    </p>
+    <Container>
+      <Triangle>
+        <h1>Hi</h1>
+      </Triangle>
+      <Content>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati dicta mollitia dolores deleniti officiis
+        autem laboriosam sequi delectus laborum, nesciunt ab adipisci, illo aliquam, fuga debitis. Sunt quod aliquam
+        adipisci voluptatem libero vitae, modi optio. Aut ipsam beatae voluptatem suscipit assumenda ratione dolor.
+        Rerum odit fuga doloribus sit, corporis exercitationem commodi ullam totam cupiditate pariatur facere eos porro
+        debitis alias ad voluptate! Assumenda vero maiores quasi sint ducimus nobis a qui eum non possimus, molestiae
+        eaque temporibus ipsam beatae! Magnam vero repudiandae repellat, ullam nesciunt quod ad exercitationem explicabo
+        ipsum voluptate dolorum maxime labore similique amet debitis eum obcaecati officia fuga ea, architecto, expedita
+        animi esse. Possimus, soluta velit! Rem pariatur nobis doloremque, odit alias repellat! Id debitis at
+        architecto!
+      </Content>
+    </Container>
   </Layout>
 );
 
