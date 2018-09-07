bfaf3e622d6689b7b19b6c490088616028dcdaed
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-09-07 17:13:27 +0200

    refactor(blog): use applause-button npm package

--- src/components/ApplauseButton/ApplauseButton.js
+++ src/components/ApplauseButton/ApplauseButton.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet';
+
+import 'applause-button';
+
 import { rhythm } from '../../utils/typography';
 
 export default class ApplauseButton extends Component {
@@ -13,18 +15,8 @@ export default class ApplauseButton extends Component {
   render() {
     return (
       <div key={this.props.location.href}>
-        <Helmet>
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/applause-button/dist/applause-button.css
-"
-          />
-          <script
-            src="https://unpkg.com/applause-button/dist/applause-button.js
-"
-          />
-        </Helmet>
         <applause-button
+          multiclap
           color="rgba(21,87,153,1)"
           style={{ width: '58px', height: '58px', zIndex: '9999', marginBottom: rhythm(1) }}
         />
