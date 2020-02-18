cbfca92c9439b661d2d8fa24f8cb29602e2ddd67
Nicky Meuleman <nicky.meuleman@gmail.com>
2020-02-18 01:27:56 +0100

    chore: styling

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,16 +1,7 @@
-// import { toTheme } from '@theme-ui/typography';
-import { tailwind } from '@theme-ui/presets';
-// import moraga from 'typography-theme-moraga';
 import nightOwl from '@theme-ui/prism/presets/night-owl.json';
+import { themeConfig } from '@nickymeuleman/gatsby-theme-blog';
 import merge from 'deepmerge';
 
-// const typographyTheme = toTheme(moraga);
-
-// export default merge(typographyTheme, {
-//   colors: {
-//     background: '#222b40',
-//     primary: '#2e3e5a',
-//     text: '#f6f6f6',
 //     // dark theme
 //     blauw1: '#222b40', // background
 //     blauw2: '#2e3e5a', // primary
@@ -31,33 +22,18 @@ import merge from 'deepmerge';
 //     grijs1: '#b4b4b4',
 //     grijs2: '#d6d6d6',
 //   },
-//   styles: {
-//     Container: {
-//       backgroundColor: 'primary',
-//     },
-//     a: {
-//       color: 'text',
-//       textDecoration: 'none',
-//       borderBottom: '1px solid',
-//       borderColor: 'blauw5',
-//     },
-//     pre: {
-//       ...nightOwl,
-//     },
-//   },
-// });
 
-const theme = merge(tailwind, {
+const theme = merge(themeConfig, {
   colors: {
     text: '#f6f6f6',
-    mutedText: `#c1c1c1c1`,
+    mutedText: `#e0e0e0e0`,
+    // mutedText: `#c1c1c1c1`,
     background: '#222b40',
     mutedBackground: '#425c86',
-    primary: '#bf1f1d',
-    mutedPrimary: '#cc4c4a',
-  },
-  sizes: {
-    lineLength: `70ch`,
+    primary: 'rgb(92, 212, 125)',
+    // primary: '#bf1f1d',
+    mutedPrimary: 'rgb(176, 251, 188)',
+    // mutedPrimary: '#cc4c4a',
   },
   fonts: {
     body:
@@ -79,76 +55,14 @@ const theme = merge(tailwind, {
       'Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
   },
   styles: {
-    root: {
-      margin: 0,
-    },
-    Layout: {
-      backgroundColor: `background`,
-      color: `text`,
-      fontFamily: `sans`, // from tailwind styles
-      fontSize: 1,
-      lineHeight: `text`,
-    },
     Header: {
       backgroundColor: `mutedBackground`,
       color: `text`,
-      fontWeight: `bold`,
-      margin: 0,
-      borderBottom: `1px solid`,
-      borderColor: `background`,
-      h1: {
-        display: `block`,
-        fontSize: 3,
-        margin: `0 auto`,
-        maxWidth: `lineLength`,
-        padding: 3,
-        width: `90vw`,
-        color: `inherit`,
-      },
-      a: {
-        color: `inherit`,
-        ':hover': {
-          color: `inherit`,
-          borderColor: `inherit`,
-        },
-      },
-    },
-    Main: {
-      margin: `0 auto`,
-      width: `90vw`,
+      borderBottom: `3px solid`,
+      borderColor: `mutedPrimary`,
     },
     pre: {
       ...nightOwl,
-      marginBottom: 2,
-      overflow: `auto`,
-      padding: 3,
-      borderRadius: `default`,
-    },
-    inlineCode: {
-      fontSize: `inherit`,
-      fontWeight: `semibold`,
-      backgroundColor: `mutedBackground`,
-      padding: 1,
-      borderRadius: `sm`,
-    },
-    a: {
-      color: `mutedPrimary`,
-      fontWeight: `bold`,
-      textDecoration: `none`,
-      ':hover': {
-        textDecoration: `none`,
-        color: `primary`,
-        borderBottomWidth: `2px`,
-        borderBottomStyle: `solid`,
-        borderBottomColor: `mutedPrimary`,
-      },
-    },
-    blockquote: {
-      margin: 0,
-      paddingLeft: 3,
-      borderLeftWidth: 5,
-      borderLeftColor: `mutedBackground`,
-      borderLeftStyle: `solid`,
     },
   },
 });
