1367e5675925e554c3d9d6e7a0e788e62581f969
Nicky Meuleman <nicky.meuleman@gmail.com>
2020-02-23 21:42:28 +0100

    feat: start index styling

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,39 +2,16 @@ import nightOwl from '@theme-ui/prism/presets/night-owl.json';
 import { themeConfig } from '@nickymeuleman/gatsby-theme-blog';
 import merge from 'deepmerge';
 
-//     // dark theme
-//     blauw1: '#222b40', // background
-//     blauw2: '#2e3e5a', // primary
-//     blauw3: '#3a5075', // primary-alt
-//     blauw6: '#222b40', // border
-//     blauw7: '#425c86', // hover
-//     wit1: 'ffffff', // icons
-//     wit2: '#f6f6f6', // text
-//     rood1: '#bf1f1d', // accent
-//     blauw5: '#4a97cc', // soft text on alt
-//     // rest
-//     blauw4: '#3375a3',
-//     rood2: '#cc4c4a',
-//     rood3: '#931816',
-//     rood4: '#cf5756',
-//     rood5: '#a91b1a',
-//     rood6: '#7d1413',
-//     grijs1: '#b4b4b4',
-//     grijs2: '#d6d6d6',
-//   },
-
 const theme = merge(themeConfig, {
   colors: {
     text: '#f6f6f6',
     mutedText: `#DFE5F3`,
-    // mutedText: `#e0e0e0e0`,
-    // mutedText: `#c1c1c1c1`,
+    mutedTextBg: `#99A8CF`,
     background: '#222b40',
     mutedBackground: '#425c86',
     primary: 'rgb(92, 212, 125)',
-    // primary: '#bf1f1d',
     mutedPrimary: 'rgb(176, 251, 188)',
-    // mutedPrimary: '#cc4c4a',
+    watermarkBg: '#4E608C',
   },
   fonts: {
     body:
@@ -63,11 +40,22 @@ const theme = merge(themeConfig, {
       borderColor: `mutedPrimary`,
     },
     PostExtra: {
-      borderColor: '#4E608C',
+      borderColor: 'watermarkBg',
     },
     PostCard: {
       '.date': {
-        color: '#99A8CF',
+        color: 'mutedTextBg',
+      },
+    },
+    HeaderLink: {
+      textDecoration: 'none',
+      color: 'text',
+      ':hover': {
+        textDecoration: `none`,
+        color: `mutedPrimary`,
+        borderBottomWidth: `2px`,
+        borderBottomStyle: `solid`,
+        borderBottomColor: `primary`,
       },
     },
     pre: {
---
+++ src/pages/index.js
@@ -0,0 +1,245 @@
+/** @jsx jsx */
+// import React from 'react';
+import { jsx } from 'theme-ui';
+import { Link, graphql } from 'gatsby';
+import { PostCard } from '@nickymeuleman/gatsby-theme-blog';
+
+const IndexPage = ({ data }) => {
+  console.log(data);
+
+  const posts = data.allBlogPost.nodes;
+  return (
+    <div
+      sx={{
+        minHeight: '100vh',
+      }}
+    >
+      <div
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '40vh',
+          minHeight: '20rem',
+          backgroundColor: 'mutedBackground',
+        }}
+      >
+        <header
+          sx={{
+            display: 'grid',
+            gridTemplateColumns:
+              'minmax(1rem, 1fr) minmax(20ch, 70ch)  minmax(1rem, 1fr)',
+            fontSize: 3,
+            py: 3,
+          }}
+        >
+          <span
+            sx={{ gridColumn: '2/3', gridRow: '1/1', justifySelf: 'start' }}
+          >
+            <Link
+              to="/"
+              sx={{
+                textDecoration: 'none',
+                color: 'text',
+              }}
+            >
+              NickyMeuleman
+            </Link>
+          </span>
+          <nav
+            sx={{
+              gridColumn: '2/3',
+              gridRow: '1/1',
+              justifySelf: 'end',
+              display: 'flex',
+              gap: 4,
+            }}
+          >
+            <span>
+              <Link
+                to="/"
+                sx={{
+                  variant: 'styles.a',
+                  border: 'none',
+                  '&.is-active': {
+                    borderBottomWidth: `2px`,
+                    borderBottomStyle: `solid`,
+                    borderBottomColor: `mutedPrimary`,
+                  },
+                }}
+                activeClassName="is-active"
+              >
+                Home
+              </Link>
+            </span>
+            <span>
+              <Link
+                to="/uses"
+                sx={{
+                  variant: 'styles.a',
+                  border: 'none',
+                  '&.is-active': {
+                    borderBottomWidth: `2px`,
+                    borderBottomStyle: `solid`,
+                    borderBottomColor: `mutedPrimary`,
+                  },
+                }}
+                activeClassName="is-active"
+              >
+                Uses
+              </Link>
+            </span>
+            <span>
+              <Link
+                to="/blog"
+                sx={{
+                  variant: 'styles.a',
+                  border: 'none',
+                  '&.is-active': {
+                    borderBottomWidth: `2px`,
+                    borderBottomStyle: `solid`,
+                    borderBottomColor: `mutedPrimary`,
+                  },
+                }}
+                activeClassName="is-active"
+              >
+                Blog
+              </Link>
+            </span>
+          </nav>
+        </header>
+        <div
+          sx={{
+            flex: 1,
+            display: 'grid',
+            gridTemplateColumns:
+              'minmax(1rem, 1fr) minmax(20ch, 70ch)  minmax(1rem, 1fr)',
+            gridTemplateRows: '1fr',
+            fontSize: 3,
+          }}
+        >
+          <div sx={{ gridColumn: '2/3', alignSelf: 'end' }}>
+            <h2
+              sx={{
+                fontSize: 4,
+                my: 0,
+                lineHeight: 1,
+                fontWeight: 'normal',
+              }}
+            >
+              Hey!
+            </h2>
+            <h1
+              sx={{
+                fontSize: 7,
+                mt: 0,
+                mb: 5,
+                lineHeight: 1,
+                fontWeight: 'normal',
+              }}
+            >
+              <span
+                sx={{
+                  color: 'mutedPrimary',
+                  fontWeight: 'bold',
+                }}
+              >
+                Build
+              </span>{' '}
+              for the modern web
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div
+        sx={{
+          display: 'grid',
+          gridTemplateColumns:
+            'minmax(1rem, 1fr) minmax(20ch, 70ch)  minmax(1rem, 1fr)',
+          fontSize: 3,
+          my: 5,
+          gap: 3,
+        }}
+      >
+        <div
+          sx={{
+            gridColumn: '2/3',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div sx={{ color: 'text' }}>Latest blogposts</div>
+          <div sx={{ textTransform: 'uppercase', color: 'mutedTextBg' }}>
+            <Link
+              to="/blog"
+              sx={{
+                textDecoration: 'none',
+                color: 'inherit',
+                letterSpacing: 'wide',
+                ':hover': {
+                  color: 'mutedPrimary',
+                },
+              }}
+            >
+              View all
+            </Link>
+          </div>
+        </div>
+        <div
+          sx={{
+            gridColumn: '2/3',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 4,
+            fontSize: 1,
+          }}
+        >
+          {posts.map(post => {
+            return (
+              <PostCard
+                key={post.id}
+                url="/"
+                title={post.title}
+                date={post.date}
+                authors={post.authors}
+                coverSizes={
+                  post.cover ? post.cover.childImageSharp.fluid : null
+                }
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const indexQuery = graphql`
+  query IndexQuery {
+    allBlogPost(
+      sort: { order: DESC, fields: date }
+      limit: 3
+      filter: { published: { eq: true } }
+    ) {
+      nodes {
+        id
+        authors {
+          shortName
+          name
+        }
+        date
+        id
+        slug
+        title
+        cover {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
