802911ab384208a8445e6a9f990eb4e1f294dbd2
Nicky Meuleman <nicky.meuleman@gmail.com>
2020-07-02 22:04:39 +0200

    fix: remove serviceworker
    site was broken since redesign because of the serviceworker that was in the old design.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,46 @@
 module.exports = {
   siteMetadata: {
-    title: 'Nicky blogs',
-    siteUrl: 'https://nickymeuleman.netlify.com',
-    description: 'Nicky tries to blog',
+    title: "Nicky blogs",
+    siteUrl: "https://nickymeuleman.netlify.com",
+    description: "Nicky tries to blog",
     social: {
-      twitter: '@NMeuleman',
+      twitter: "@NMeuleman",
     },
   },
   plugins: [
     {
-      resolve: '@nickymeuleman/gatsby-theme-blog',
+      resolve: "@nickymeuleman/gatsby-theme-blog",
       options: {
-        basePath: 'blog',
-        contentPath: 'data/posts',
-        assetPath: 'data/assets',
+        basePath: "blog",
+        contentPath: "data/posts",
+        assetPath: "data/assets",
       },
     },
-    'gatsby-plugin-theme-ui',
-    'gatsby-plugin-extract-schema',
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-extract-schema",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Nicky Meuleman',
-        short_name: 'NickyM',
-        start_url: '/',
-        background_color: '#222b40',
-        theme_color: 'rgb(176, 251, 188)',
-        display: 'minimal-ui',
-        icon: 'static/favicon.svg',
+        name: "Nicky Meuleman",
+        short_name: "NickyM",
+        start_url: "/",
+        background_color: "#222b40",
+        theme_color: "rgb(176, 251, 188)",
+        display: "minimal-ui",
+        icon: "static/favicon.svg",
       },
     },
+    "gatsby-plugin-remove-serviceworker",
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'UA-117805994-1',
+        trackingId: "UA-117805994-1",
         head: false,
         anonymize: true,
         respectDNT: true,
       },
     },
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-netlify', // keep as last in array
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-netlify", // keep as last in array
   ],
 };
