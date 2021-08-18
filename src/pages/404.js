4b3ac39aee984f701018cf8e96d13adb95ffcae9
Nicky Meuleman <nicky.dev@outlook.com>
2021-08-18 22:18:51 +0200

    feat: added a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,37 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { Link } from "gatsby";
+
+const NotFoundPage = () => (
+  <div
+    sx={{
+      height: "100vh",
+      paddingLeft: 6,
+      paddingTop: 5,
+      fontSize: 4,
+      fontFamily:
+        '"Segoe UI", Roboto, "Helvetica Neue", Ubuntu, Arial, sans-serif',
+    }}
+  >
+    <h1 sx={{ fontSize: "8rem", marginTop: 0 }}>:(</h1>
+    <h2>Woops!</h2>
+    <p sx={{ my: 2 }}>This page was not found.</p>
+    <p sx={{ my: 2 }}>
+      I&apos;m not sure how you got here, but you are now looking at an
+      uninteresting, empty page.
+    </p>
+    <div sx={{ fontSize: 3, mt: 5 }}>
+      {"Check out "}
+      <Link to="/" sx={{ variant: "styles.a" }}>
+        the home page
+      </Link>
+      {" or "}
+      <Link to="/blog" sx={{ variant: "styles.a" }}>
+        the blog
+      </Link>
+      {" instead."}
+    </div>
+  </div>
+);
+
+export default NotFoundPage;
