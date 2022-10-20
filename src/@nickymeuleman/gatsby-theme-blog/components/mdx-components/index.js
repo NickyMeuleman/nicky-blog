9033fe052c03ca12ddf6e497c8940ab021d25d7f
Nicky Meuleman <nicky.dev@outlook.com>
2022-10-20 15:36:52 +0200

    chore: add math posts back

--- src/@nickymeuleman/gatsby-theme-blog/components/mdx-components/index.js
+++
@@ -1,4 +0,0 @@
-export { Aside } from "../../../../components/Aside";
-export { BlockQuote as blockquote } from "../../../../components/BlockQuote";
-export { Div as div } from "../../../../components/Div";
-export { Span as span } from "../../../../components/Span";
--- src/components/Div.js
+++ src/components/Div.js
@@ -4,30 +4,7 @@ import { MathBlock } from "./MathBlock";
 
 const Div = ({ className, children, ...rest }) => {
   if (className.includes(`math-display`)) {
-    const validProps = [`title`];
-    let math = children;
-    let metaString = null;
-
-    // check if the first line was a metaString
-    const [firstLine, ...restOfLines] = children.split(`\n`);
-    if (validProps.some((propName) => firstLine.includes(propName))) {
-      math = restOfLines.join(`\n`);
-      metaString = firstLine;
-    }
-
-    const metaObj =
-      metaString &&
-      metaString.split(` `).reduce((acc, cur) => {
-        if (cur.split(`=`).length > 1) {
-          const [key, value] = cur.split(`=`);
-          acc[key] = value;
-          return acc;
-        }
-        acc[cur] = true;
-        return acc;
-      }, {});
-
-    return <MathBlock {...metaObj} math={math} {...rest} />;
+    return <MathBlock math={children} {...rest} />;
   }
   return (
     <div className={className} {...rest}>
