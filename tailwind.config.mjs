92d1fbbe7739fc5828e6d6482a6e590e40d2c8c6
Nicky Meuleman <nicky.dev@outlook.com>
2024-11-27 20:17:47 +0100

    feat: tailwind features like the typography plugin and a custom anchor component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
+import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -25,7 +28,65 @@ export default {
       content: {
         checkmark: "'\\2713'",
       },
+      typography: {
+        // no! bad! don't things
+        DEFAULT: {
+          css: {
+            code: {
+              color: "inherit",
+            },
+            "code::before": {
+              content: "",
+            },
+            "code::after": {
+              content: "",
+            },
+            strong: {
+              color: "inherit",
+            },
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addBase, addComponents, theme }) => {
+      addBase({
+        p: {
+          marginTop: theme("margin.4"),
+          marginBottom: theme("margin.4"),
+        },
+      });
+      addComponents({
+        ".anchor": {
+          "--underlineWidth": theme("borderWidth.2"),
+          "--blockLength": theme("spacing.3"),
+          color: theme("colors.mutedText"),
+          textDecoration: `none`,
+          backgroundImage: `linear-gradient(90deg, ${theme("colors.background")}, ${theme("colors.background")}), linear-gradient(${theme("colors.mutedPrimary")}, ${theme("colors.mutedPrimary")}), linear-gradient(${theme("colors.primary")}, ${theme("colors.primary")})`,
+          backgroundRepeat: `no-repeat`,
+          backgroundSize: `var(--blockLength) var(--underlineWidth), 100% var(--underlineWidth), 0 var(--underlineWidth)`,
+          backgroundPosition: `calc(var(--blockLength) * -1) 100%, 100% 100%, 0 100%`,
+          transition: `background-size cubic-bezier(0.39, 0.575, 0.565, 1) 0.3s, background-position cubic-bezier(0.39, 0.575, 0.565, 1) 0.3s, color cubic-bezier(0.39, 0.575, 0.565, 1) 0.3s`,
+          border: `none`,
+          "&:hover": {
+            color: `mutedPrimary`,
+            border: `none`,
+            backgroundSize: `var(--blockLength) var(--underlineWidth), 0 var(--underlineWidth), 100% var(--underlineWidth)`,
+            backgroundPosition: `calc(100% + var(--blockLength)) 100%, 100% 100%, 0 100%`,
+          },
+        },
+        ".code": {
+          fontSize: "0.85em",
+          overflowWrap: "break-word",
+          fontFamily: theme("fontFamily.mono"),
+          letterSpacing: theme("letterSpacing.wide"),
+          backgroundColor: theme("colors.mutedBackground"),
+          padding: theme("padding.1"),
+          borderRadius: theme("borderRadius.sm"),
+        },
+      });
+    }),
+    typography(),
+  ],
 };
