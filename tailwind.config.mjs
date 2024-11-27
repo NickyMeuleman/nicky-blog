475423d59a6f41a5bd15116525b0ffbf52aa23aa
Nicky Meuleman <nicky.dev@outlook.com>
2024-11-27 20:28:17 +0100

    fix: inherit blockquote color in posts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -44,6 +44,9 @@ export default {
             strong: {
               color: "inherit",
             },
+            blockquote: {
+              color: "inherit",
+            },
           },
         },
       },
