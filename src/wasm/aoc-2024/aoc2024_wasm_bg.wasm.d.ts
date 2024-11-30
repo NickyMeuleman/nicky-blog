67121110e2ca2f17afa26127c2725fe976bd014f
Nicky Meuleman <nicky.dev@outlook.com>
2024-11-30 22:09:16 +0100

    feat: prep for aoc2024

---
+++ src/wasm/aoc-2024/aoc2024_wasm_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function solve(a: number, b: number, c: number, d: number): number;
+export function __wbg_wasmsolution_free(a: number): void;
+export function __wbg_get_wasmsolution_part1(a: number, b: number): void;
+export function __wbg_set_wasmsolution_part1(a: number, b: number, c: number): void;
+export function __wbg_get_wasmsolution_part2(a: number, b: number): void;
+export function __wbg_set_wasmsolution_part2(a: number, b: number, c: number): void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function __wbindgen_export_1(a: number, b: number, c: number): void;
+export function __wbindgen_export_2(a: number, b: number): number;
+export function __wbindgen_export_3(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_export_4(a: number): void;
+export function __wbindgen_export_5(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_export_6(a: number, b: number, c: number): void;
